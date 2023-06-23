2af4e3de2cc9c688b6d3a9635b3746d74e4ef63c
Francis <mingomba@gmail.com>
2023-06-23 06:04:14 +0000

    Working on post request, add user to database

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+
+const Joi = require('joi');
+const users = require('./routes/users');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Francis');
-})
 
+mongoose.connect('mongodb://localhost/nodejsDemoDB')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDb'));
 
-
-
-
+app.use(express.json());
+app.use('/api/users', users);
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,2 +1,37 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    }
+}));
+
+function validate(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(50).required(),
+        password: Joi.string().min(5).max(50).required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validate;
