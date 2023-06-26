9d4f70f0d3705c192469098ed4cd4875ca8fa464
Francis <mingomba@gmail.com>
2023-06-26 02:35:17 +0000

    Working on post api

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,9 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    let _user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('user already exist');
+
     let user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -25,63 +28,51 @@ router.post('/', async (req, res) => {
     res.send(user);
 });
 
-/*async function updateUser(id) {
-    const user = await User.findByIdAndUpdate(id, {
-        $set: {
-
-            name: 'simonia',
-            email: 'ming@gmail.com',
-            password: 'new passwoed'
-            //name: req.body.name,
-            //email: req.body.email,
-            //password: req.body.password
-        }
-    }, { new: true });
-    console.log(user);
-}
-updateUser('64952bb21204864ec993af5b');
-
-*/
-
-
 router.put('/:id', async (req, res) => {
-    const userInDb = await User.find(c => c.id === parseInt(req.params.id));
-    if (!userInDb) res.status(400).send('The user with the given id was not found ');
-
     const { error } = validate(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    userInDb.name = req.body.name;
-    userInDb.email = req.body.email;
-    userInDb.password = req.body.password;
-    res.send(userInDb);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    const user = await User.findByIdAndUpdate(req.params.id, { name: req.body.name, email: req.body.email, password: req.body.password }, {
+        new: true
+    });
 
+    if (!user) return res.status(404).send('The user with the given ID was not found.');
+
+    res.send(user);
 });
 
-
-/*
-  
-app.delete('/api/genres/:id', (req, res) => {
-const genre = genres.find(c => c.id === parseInt(req.params.id));
-if (!genre) return res.status(404).send('The genre with the given ID was not found.');
- 
-const index = genres.indexOf(genre);
-genres.splice(index, 1);
- 
-res.send(genre);
-});
- 
 router.get('/:id', async (req, res) => {
-    const userInDb = await User.find(c => c.id === parseInt(req.params.id));
-    if (!userInDb) return res.status(404).send('The genre with the given ID was not found.');
-    res.send(userInDb);
+    const user = await User.findById(req.params.id);
+
+    if (!user) return res.status(404).send('The genre with the given ID was not found.');
+
+    res.send(user);
 });
 
+router.delete('/:id', async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.id);
 
-*/
+    if (!user) return res.status(404).send('The genre with the given ID was not found.');
 
+    res.send(user);
+}); router.delete('/:id', async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.id);
+
+    if (!user) return res.status(404).send('The genre with the given ID was not found.');
+
+    res.send(user);
+});
+
+//----------------------------------------------------------------------
+
+router.delete('/:id', async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.id);
+
+    if (!user) return res.status(404).send('The user with the given ID was delete.');
+
+    res.send(user);
+});
+
+//----------------------------------------------------------------------
 
 module.exports = router;
