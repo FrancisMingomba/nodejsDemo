8e71ee790d9b6641ce176e4ce66862da6525c62b
Francis <mingomba@gmail.com>
2023-07-06 05:44:42 +0000

    Other commit

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,7 @@ const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User} = require('../models/user');
-//const mongoose = require('mongoose');
 const express = require('express');
-//const { result } = require('lodash');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
